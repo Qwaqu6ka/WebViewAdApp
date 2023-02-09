f64392d76a29d56a20dbca4bbb1965b015355be2
Bortsov Roman <r.bortsov@yandex.ru>
2023-02-09 16:43:19 +1000

    Добавлена проверка на наличие ссылки, наличие интернета, сделан главный экран с web view и сообщениями об ошибках

--- app/src/main/java/com/example/webviewadapp/MainActivity.kt
+++ app/src/main/java/com/example/webviewadapp/MainActivity.kt
@@ -1,11 +1,169 @@
 package com.example.webviewadapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.telephony.TelephonyManager
+import android.view.View
+import android.webkit.CookieManager
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.example.webviewadapp.databinding.ActivityMainBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import java.util.*
+
+
+private const val PREF_NAME = "com.example.webviewadapp"
+private const val LINK_KEY = "linkKey"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var link: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState != null) {
+            binding.webView.restoreState(savedInstanceState)
+        } else {
+            initWebView()
+            link = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE).getString(LINK_KEY, "")
+                .toString()
+
+            if (link != "") {
+                showPage(link)
+            } else if (isDeviceEmulator() || isSimAbsent()) {
+                // TODO showCap()
+            } else {
+                showPage("https://www.google.ru/")
+//                goToDatabase()
+            }
+        }
+        binding.tryAgainButton.setOnClickListener { showPage(link) }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        binding.webView.saveState(outState)
+    }
+
+    private fun showPage(link: String) {
+        hideInternetLackMessage()
+        if (isInternetAvailable()) {
+            binding.webView.loadUrl(link)
+        } else {
+            showInternetLackMessage()
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView() {
+        binding.webView.webViewClient = WebViewClient()
+        val webSettings = binding.webView.settings
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.javaScriptCanOpenWindowsAutomatically = true
+        webSettings.databaseEnabled = true
+        webSettings.setSupportZoom(false)
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.setAcceptCookie(true)
+    }
+
+    private fun showInternetLackMessage() {
+        binding.webView.visibility = View.GONE
+        binding.internetLackLayout.visibility = View.VISIBLE
+    }
+
+    private fun hideInternetLackMessage() {
+        binding.webView.visibility = View.VISIBLE
+        binding.internetLackLayout.visibility = View.GONE
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        } else {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+            }
+        }
+        return false
+    }
+
+    private fun isDeviceEmulator(): Boolean {
+        if (BuildConfig.DEBUG) return false // when developer use this build on emulator
+        val phoneModel = Build.MODEL
+        val buildProduct = Build.PRODUCT
+        val buildHardware = Build.HARDWARE
+        var result = (Build.FINGERPRINT.startsWith("generic")
+                || phoneModel.contains("google_sdk")
+                || phoneModel.lowercase(Locale.getDefault()).contains("droid4x")
+                || phoneModel.contains("Emulator")
+                || phoneModel.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || buildHardware == "goldfish"
+                || Build.BRAND.contains("google")
+                || buildHardware == "vbox86"
+                || buildProduct == "sdk"
+                || buildProduct == "google_sdk"
+                || buildProduct == "sdk_x86"
+                || buildProduct == "vbox86p"
+                || Build.BOARD.lowercase(Locale.getDefault()).contains("nox")
+                || Build.BOOTLOADER.lowercase(Locale.getDefault()).contains("nox")
+                || buildHardware.lowercase(Locale.getDefault()).contains("nox")
+                || buildProduct.lowercase(Locale.getDefault()).contains("nox"))
+        if (result) return true
+        result = (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+        if (result) return true
+        result = "google_sdk" == buildProduct
+        return result
+    }
+
+    private fun isSimAbsent(): Boolean {
+        val telManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+        if (telManager.simState == TelephonyManager.SIM_STATE_ABSENT)
+            return true
+        return false
+    }
+
+    private fun goToDatabase() {
+        try {
+            val remoteConfig = Firebase.remoteConfig
+            val configSettings = remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 3600
+            }
+            remoteConfig.setConfigSettingsAsync(configSettings)
+            remoteConfig.setDefaultsAsync(mapOf("url" to ""))
+            // TODO ...
+        } catch (e: Exception) {
+            showDatabaseError(e.message)
+        }
+    }
+
+    private fun showDatabaseError(message: String?) {
+        binding.errorTextView.text = message
+        binding.webView.visibility = View.GONE
+        binding.errorLayout.visibility = View.VISIBLE
     }
 }
