bffb81588fd2c66a53ed6dc941d83fb2bacea6a5
Bortsov Roman <r.bortsov@yandex.ru>
2023-02-11 01:11:34 +1000

    Добавлена заглушка спортивных новостей(пока без изображений)

---
+++ app/src/main/java/com/example/webviewadapp/api/NewsFetcher.kt
@@ -0,0 +1,17 @@
+package com.example.webviewadapp.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NewsFetcher {
+
+    companion object {
+        val getApi: SportNewsApi by lazy {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://newsapi.org/")
+                .build()
+            retrofit.create(SportNewsApi::class.java)
+        }
+    }
+}
